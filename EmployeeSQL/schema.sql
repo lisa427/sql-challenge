2a04b3119e66f1c8f3924f5886b86b1de3f0e1fe
lisa427 <lisa.mcgowan4@gmail.com>
2020-11-06 23:30:29 -0500

    added schema

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,51 @@
+CREATE TABLE titles (
+    title_id VARCHAR PRIMARY KEY,
+    title VARCHAR
+     );
+
+CREATE TABLE employees (
+    emp_no INTEGER PRIMARY KEY,
+    emp_title_id VARCHAR,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+    birth_date DATE,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    sex CHAR(1),
+    hire_date DATE
+    );
+
+CREATE TABLE salaries (
+    emp_no INTEGER,
+    salary INTEGER
+    );
+
+CREATE TABLE departments (
+    dept_no VARCHAR PRIMARY KEY,
+    dept_name VARCHAR
+    );
+
+CREATE TABLE dept_emp (
+    emp_no INTEGER,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    dept_no VARCHAR,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+    );
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    emp_no INTEGER,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    PRIMARY KEY (dept_no, emp_no)
+    );
+
+ALTER TABLE salaries 
+ADD COLUMN salary_id SERIAL;
+
+ALTER TABLE salaries 
+ADD PRIMARY KEY (salary_id);
+
+ALTER TABLE salaries 
+ADD FOREIGN KEY (emp_no) 
+REFERENCES employees (emp_no);
